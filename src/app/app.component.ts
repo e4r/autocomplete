558e90470ad2db3a36ff1eb5d42697737217d684
Leonardo Maria Miliacca <leo.mmcm@gmail.com>
2019-03-28 19:24:29 +0100

    allineamento

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +8,55 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'autocomplete';
+
+  // un'istanza di FormGroup per ora solo dichiarata
+  form: FormGroup;
+
+  // queste sono le opzioni per ora solo dichiarate, ma che dovrò usare quando avrò digitato
+  remoteOptions: any[];
+
+  cibi: string[] = ['Zucchine', 'Arrosticini', 'Pane', 'Gelato'];
+
+  constructor(
+    private fb: FormBuilder,
+    private http: HttpClient
+  ) {
+    this.buildForm();
+  }
+
+  /**
+   * Qui istanzio il {@link FormGroup}, ed è qui che assegno i validatori
+   */
+  buildForm(): void {
+    this.form = this.fb.group({
+      autocompleteField: [null, Validators.required],
+      exampleCheckbox: [null, Validators.requiredTrue],
+      exampleSelect: [null]
+    });
+  }
+
+  /**
+   * Questo metodo effettua la ricerca e popola il risultato
+   * @param $event
+   */
+  search($event): void {
+    const word = $event.target.value;
+    this.http.get('https://jsonplaceholder.typicode.com/posts/')
+    .subscribe((posts: any[]) => {
+      /**
+       * per velocità, non effettuo sull'URL il filtro ma una volta che
+       * ho ricevuto i dati.
+       */
+      this.remoteOptions = posts.filter((post) => {
+        return !!post.title.match(new RegExp(word, 'gi'));
+      });
+
+      // una volta popolato `remoteOptions` non dovrete più preoccuparvi di gestire
+      // il flusso
+
+    }, err => {
+      // qui va gestito l'errore nella chiamata
+    });
+  }
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,40 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
+import { CommonModule } from '@angular/common';
+import {MatAutocompleteModule} from '@angular/material/autocomplete';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import {MatInputModule} from '@angular/material/input';
+import {MatCheckboxModule} from '@angular/material/checkbox';
+import {MatSelectModule} from '@angular/material';
+import {HttpClientModule} from '@angular/common/http';
+import {MatButtonModule} from '@angular/material/button';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    /* moduli fondamentali per gestire i FormGroup */
+    FormsModule,
+    ReactiveFormsModule,
+
+    /* Qui invece importo i moduli necessari per material design */
+    MatFormFieldModule,
+    MatAutocompleteModule,
+    MatInputModule,
+    MatCheckboxModule,
+    MatButtonModule,
+    MatSelectModule,
+
+    /* questo modulo http andrebbe utilizzato solo in dei service */
+    HttpClientModule,
+
+    CommonModule,
+    BrowserModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
